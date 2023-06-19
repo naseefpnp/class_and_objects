8c932b03c646a72e20b469136ca9a065094577ea
naseefpnp <naseefpnp098@gmail.com>
2023-06-20 03:02:29 +0530

    operations1

---
+++ bin/operations1.dart
@@ -0,0 +1,21 @@
+///1. arithmetic opration +,-,*,/,%,~/
+void main(){
+    int a=100;
+    int b=35;
+
+    print("a + b=${a+b}");
+    print("a - b=${a-b}");
+    print("a * b=${a*b}");
+    print("a / b=${a/b}");
+    print("a % b=${a%b}");//to fatch remaider
+    print("a ~ b=${a~/b}");
+
+    ///2.ASSIGMENT OPERATOR =, +=,-=,*= ..........etc
+    dynamic x =20, y =30;
+    print("x = y  -> ${x=y}");
+    print("x += y  -> ${x+=y}");
+    print("x-= y -> ${x-=y}");
+    print("x *= y -> ${x*=y}");
+    print("x /= y -> ${x/=y}");
+
+}
