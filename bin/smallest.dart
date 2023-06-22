3963cfb27c28937ae87ef91cca0dc6ece1d16a4b
naseefpnp <naseefpnp098@gmail.com>
2023-06-22 07:31:10 +0530

    smallest

--- bin/smallest.dart
+++ bin/smallest.dart
@@ -1,10 +1,24 @@
 import 'dart:io';
 
 void main (){
-  print("enter three numbers:");
+ /// print("enter three numbers:");
+ /// int n1 = int.parse(stdin.readLineSync()!);
+ /// int n2 = int.parse(stdin.readLineSync()!);
+ /// int n3 = int.parse(stdin.readLineSync()!);
+ /// var smallest = (n1 < n2 ? (n1 < n3 ? n1 : n3) : (n2 < n3 ? n2 : n3));
+ /// print("the smallest number is = ${smallest}");
+ print("enter three numbers:");
   int n1 = int.parse(stdin.readLineSync()!);
   int n2 = int.parse(stdin.readLineSync()!);
   int n3 = int.parse(stdin.readLineSync()!);
-  var smallest = (n1 < n2 ? (n1 < n3 ? n1 : n3) : (n2 < n3 ? n2 : n3));
-  print("the smallest number is = ${smallest}");
+  if(n1 < n2 && n1 < n3 ){
+    print("smallest number is ${n1}");
+  }
+  else if(n2 < n3){
+    print("smallest number is ${n2}");
+  }
+  else{
+    print("smallest number is ${n3}");
+  }
+
 }
