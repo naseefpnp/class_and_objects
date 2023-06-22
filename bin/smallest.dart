436df0bac503438e4297157ea8b2d2a345882228
naseefpnp <naseefpnp098@gmail.com>
2023-06-22 07:45:16 +0530

    small

--- bin/smallest.dart
+++ bin/smallest.dart
@@ -1,12 +1,24 @@
 import 'dart:io';
 
 void main (){
- /// print("enter three numbers:");
- /// int n1 = int.parse(stdin.readLineSync()!);
- /// int n2 = int.parse(stdin.readLineSync()!);
- /// int n3 = int.parse(stdin.readLineSync()!);
- /// var smallest = (n1 < n2 ? (n1 < n3 ? n1 : n3) : (n2 < n3 ? n2 : n3));
- /// print("the smallest number is = ${smallest}");
+ /* print("enter three numbers:");
+  int n1 = int.parse(stdin.readLineSync()!);
+  int n2 = int.parse(stdin.readLineSync()!);
+  int n3 = int.parse(stdin.readLineSync()!);
+  var smallest = (n1 < n2 ? (n1 < n3 ? n1 : n3) : (n2 < n3 ? n2 : n3));
+  print("the smallest number is = ${smallest}");*/
+ 
+
+ /* print("enter two numbers:");
+  int n1 = int.parse(stdin.readLineSync()!);
+  int n2 = int.parse(stdin.readLineSync()!);
+  if(n1 < n2){
+    print("smallest number is ${n1}");
+  }else{
+    print("smallest number is ${n2}");
+  }*/
+
+
  print("enter three numbers:");
   int n1 = int.parse(stdin.readLineSync()!);
   int n2 = int.parse(stdin.readLineSync()!);
